10d7a6494aec48e7782ca1a13339ec21e041f7b3
Thomas Pring <pringthomas4@gmail.com>
2019-10-01 01:20:43 -0400

    Added basic UI Manager and Empty MainMenu Script
    Created the basic UI Manager script with Quit and show screen functionality. Also changed the Engine default editor to VS2019

---
+++ Filthy Casuals Production/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    #region MonoBehavior
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Quit()
+    {
+        UIManager.main.Quit();
+    }
+
+    #endregion
+}
---
+++ Filthy Casuals Production/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Static Members
+    
+    public static UIManager main;
+
+    #endregion
+
+    #region Runtime Members
+
+    // Screen struct
+    [System.Serializable]
+    public struct Screen
+    {
+        public string name;
+        public GameObject screen;
+        public Animator screenAnimator;
+    }
+
+    // List of screens to iterate through
+    public List<Screen> Screens = new List<Screen>();
+
+    // Counter int to track the current screen in the list
+    public int CurrentScreen;
+
+    #endregion
+
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        // Singleton behavior
+        if (main == null)
+        {
+            main = this;
+        }
+        else
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    // Displays the screen with the given name
+    public void ShowScreen(string name)
+    {
+        for (int i = 0; i < Screens.Count; i++)
+        {
+            if (Screens[i].name.Equals(name))
+            {
+                StartCoroutine(ShowScreenCoroutine(i));
+            }
+        }
+    }
+
+    // Coroutine to play screen animation
+    public IEnumerator ShowScreenCoroutine(int index)
+    {
+        if (Screens[CurrentScreen].screenAnimator != null)
+        {
+            Screens[CurrentScreen].screenAnimator.SetTrigger("Close");
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        Screens[CurrentScreen].screen.SetActive(false);
+        Screens[index].screen.SetActive(true);
+        CurrentScreen = index;
+    }
+
+    // Quits application
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Game");
+    }
+
+    #endregion
+
+    #region Private Methods
+
+
+
+    #endregion
+}
