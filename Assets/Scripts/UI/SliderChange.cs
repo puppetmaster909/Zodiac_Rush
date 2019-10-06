9fb4b577191cd033928434af21aeeb64ec3bf898
SamusAranM <mbarahna210@gmail.com>
2019-10-05 21:29:23 -0400

    Slider
    Just added a score bar that increase when you press Space. Still need to apply to the actual gameplay though

---
+++ Assets/Scripts/UI/SliderChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderChange : MonoBehaviour
+{
+    private float currentScore;
+    public float addScore;
+    public float maxScore;
+    public Slider slider;
+
+    void Update()
+    {
+        slider.value = CalculateSliderValue();
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentScore >= 0)
+            {
+                currentScore += addScore;
+            }
+
+        }
+
+        if (currentScore == maxScore)
+        {
+            //add scene manager here 
+
+            Debug.Log("Level Complete!");
+        }
+
+    }
+
+    float CalculateSliderValue()
+    {
+        return (currentScore / maxScore);
+    }
+
+}
