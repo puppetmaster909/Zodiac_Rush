bddafadfd093a7288065f9a2380dee3a3f7b3f4a
Thomas Pring <pringthomas4@gmail.com>
2019-10-05 22:25:01 -0400

    Revert "Merge branch 'master' into Erik"
    This reverts commit c457fbd886c33ec238763f8150430cc33f616c19.

---
+++ Assets/Scripts/Background_Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Tile : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        
+    }
+}
