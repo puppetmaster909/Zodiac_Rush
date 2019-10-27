b46dc4c48ffaf7b329a75b32eb0f454f2d218a97
Erik M <37202475+CagedAlchemist@users.noreply.github.com>
2019-10-27 18:52:15 -0400

    Added Camera Manager

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -6,29 +6,35 @@ public class CameraManager : MonoBehaviour
 {
     private static CameraManager main;
 
-    private void Awake()
+    private Board board;
+    public float cameraOffset;
+    public float aspectRatio = 0.625f;
+    public float padding = 2;
+    public float yOffset = 1;
+
+    // Use this for initialization
+    void Start()
     {
-        // Singleton behavior
-        if (main == null)
+        board = FindObjectOfType<Board>();
+        if (board != null)
         {
-            main = this;
+            RepositionCamera(board.width - 1, board.height - 1);
+        }
+    }
+
+    void RepositionCamera(float x, float y)
+    {
+        Vector3 tempPosition = new Vector3(x / 2, y / 2 + yOffset, cameraOffset);
+        transform.position = tempPosition;
+        if (board.width >= board.height)
+        {
+            Camera.main.orthographicSize = (board.width / 2 + padding) / aspectRatio;
         }
         else
         {
-            GameObject.Destroy(gameObject);
+            Camera.main.orthographicSize = (board.height / 2 + padding) + 2 * yOffset;
         }
-        
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
