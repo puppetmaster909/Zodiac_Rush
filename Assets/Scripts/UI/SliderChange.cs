f5d845aa9e07a03481df16b93be6065599ee2ebd
Erik M <37202475+CagedAlchemist@users.noreply.github.com>
2019-11-03 18:22:02 -0500

    Animated Zodiac WIP
    -Changed Layer Order of Animated Cat (Shouldn't appear over pause menu anymore)
    -Trapped Zodiac Moves to Center of Board after victory

--- Assets/Scripts/UI/SliderChange.cs
+++ Assets/Scripts/UI/SliderChange.cs
@@ -11,7 +11,7 @@ public class SliderChange : MonoBehaviour
 
     public float maxScore;
     public Slider slider;
-
+    public GameObject TrappedZodiac;
     private Board theBoard;
 
     // Maria Edit Part 33 - Scoring System
@@ -50,7 +50,36 @@ public class SliderChange : MonoBehaviour
     }
 
     #endregion
-
+    IEnumerator ShowVictoryScreen(float time)
+    {
+        StartCoroutine(MoveOverSeconds(TrappedZodiac, new Vector3(2.9f, 3.1f, 40.0f), 2.5f));
+        yield return new WaitForSeconds(time);
+        UIManager.main.ShowScreen("Victory");
+        Debug.Log("Waiting: " + time + "seconds");
+    }
+    public IEnumerator MoveOverSpeed(GameObject objectToMove, Vector3 end, float speed)
+    {
+        // speed should be 1 unit per second
+        while (objectToMove.transform.position != end)
+        {
+            objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, end, speed * Time.deltaTime);
+            Debug.Log(objectToMove.transform.position);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    public IEnumerator MoveOverSeconds(GameObject objectToMove, Vector3 end, float seconds)
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = objectToMove.transform.position;
+        while (elapsedTime < seconds)
+        {
+            objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+            Debug.Log(objectToMove.transform.position);
+        }
+        objectToMove.transform.position = end;
+    }
     #region Public Methods
 
     // Maria Edit
@@ -86,7 +115,8 @@ public class SliderChange : MonoBehaviour
                         }
                     }
                     Debug.Log("Level Complete!");
-                    UIManager.main.ShowScreen("Victory");
+                    StartCoroutine(ShowVictoryScreen(10f));
+                    
                 }
             }
         }
